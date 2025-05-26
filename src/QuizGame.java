274b5c75668e3f1f2620c593a5a3b1b390b6594d
andre <andrem166@gmail.com>
2025-05-26 20:36:35 -0300

    Perguntas adicionadas

--- src/QuizGame.java
+++ src/QuizGame.java
@@ -5,20 +5,14 @@ public class QuizGame {
     Scanner scan = new Scanner(System.in);
     String jogarNovamente = "S";
 
+    List<List<String>> perguntas = GerenciadorDePerguntas.getTodasAsPerguntas();
+
     while (jogarNovamente.equals("S")) {
-      boolean isLetraCerta = false;
+      boolean isLetraCerta;
       boolean isSimOuNao;
       int pontuacao = 0;
       int count = 0;
       String respostaUsuario = "";
-
-      // Criando perguntas e respostas usando List
-      List<List<String>> perguntas = new ArrayList<>();
-      perguntas.add(Arrays.asList("O que significa a sigla HTML?", "HyperText Markup Language", "HighText Machine Language", "Hyperlink and Text Management Language", "Home Tool Markup Language"));
-      perguntas.add(Arrays.asList("Qual destes é um framework JavaScript para front-end?", "React", "Django", "Laravel", "Spring"));
-      perguntas.add(Arrays.asList("Qual linguagem é mais comumente usada para desenvolvimento de aplicativos Android nativos?", "Kotlin", "Swift", "JavaScript", "Ruby"));
-      perguntas.add(Arrays.asList("O que faz a função `len()` em Python?", "Retorna o tamanho de um objeto", "Concatena duas strings", "Cria uma lista vazia", "Converte um número em string"));
-      perguntas.add(Arrays.asList("Qual destas é uma característica do paradigma de programação orientada a objetos?", "Encapsulamento", "Programação Funcional", "Recursividade", "Programação Lógica"));
 
       List<Integer> indices = new ArrayList<>();
       for (int i = 0; i < perguntas.size(); i++) {
@@ -29,7 +23,6 @@ public class QuizGame {
       System.out.println("-----------------------------------");
       System.out.println("Bem-Vindo ao Perguntas e Respostas!");
       System.out.println("-----------------------------------");
-
 
       for (int i = 0; i < perguntas.size(); i++) {
         int perguntaIndex = indices.get(i); // Pega uma pergunta aleatória sem repetição
@@ -50,6 +43,7 @@ public class QuizGame {
           }
         }
         do {
+          isLetraCerta = false;
           System.out.print("Digite a letra da resposta correta: ");
           respostaUsuario = validaLetra(scan.next());
           for(int j = 0; j < letras.length; j++) {
@@ -63,9 +57,6 @@ public class QuizGame {
           }
         } while (!isLetraCerta);
 
-
-
-
         if (respostaUsuario.equals(respostaCorreta)) {
           System.out.println("Certa a resposta!!!!!");
           pontuacao++;
@@ -75,14 +66,13 @@ public class QuizGame {
 
         System.out.println("-------------------------------------------");
         count++;
-        if (count >= 2) {
+        if (count >= 5) {
           System.out.println("Fim de Jogo!");
           break;
         }
       }
 
       System.out.println("Sua pontuação foi de: " + pontuacao + " pontos");
-
 
       do {
         System.out.println("Deseja jogar novamente? (s/n)");
@@ -94,13 +84,9 @@ public class QuizGame {
            System.out.println("Você não digitou S ou N");
          }
       } while (!isSimOuNao);
-
     }
-
-
     scan.close();
   }
-
   public static String validaLetra(String letra) {
     return String.valueOf(letra.toUpperCase().charAt(0));
   }
